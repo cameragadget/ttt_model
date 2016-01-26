f1d128989f5342bda443c3c5669446aefc90c8c0
jonah sobol <jonah.sobol@gmail.com>
2016-01-26 12:59:14 -0800

    Phil's TTT

--- main.js
+++ main.js
@@ -12,6 +12,25 @@ var board = [
 ];
 
 // 1. START THE GAME
+//   inputs:
+//    - current player, board, won or not
+//
+//   output:
+//    - board is emptied, player is X, won is false
+
+var startGame = function() {
+won = false;
+player = "X"
+board = [
+"", "", "",
+"", "", "",
+"", "", ""
+];
+};
+
+
+
+
 // 2. PLAYER MOVE (CHANGES TURN)
 //   inputs:
 //    - whose turn it is,
@@ -38,4 +57,40 @@ var move = function(cellIndex) {
 
 
 // 3. WINNING
-var gameWon = function() {};
+//   inputs:
+//     - board
+//
+//   outputs:
+//    - true or false
+//
+// you can replace "if" with "return" and eliminate
+// the return true/false down below
+// because it's only ever returning true/false!!!
+//
+
+var gameWon = function() {
+  if (((board[0] === board[1]) && (board[0] === board[2]) && (board[0] !== "")) ||
+     ((board[3] === board[4]) && (board[3] === board[5]) && (board[4] !== "")) ||
+     ((board[6] === board[7]) && (board[6] === board[7]) && (board[6] !== "")) ||
+     ((board[0] === board[3]) && (board[0] === board[6]) && (board[0] !== "")) ||
+     ((board[1] === board[4]) && (board[1] === board[8]) && (board[1] !== "")) ||
+     ((board[2] === board[5]) && (board[3] === board[8]) && (board[2] !== "")) ||
+     ((board[0] === board[4]) && (board[8] === board[2]) && (board[0] !== "")) ||
+     ((board[6] === board[4]) && (board[6] === board[2]) && (board[6] !== ""))){
+    return true;
+  } else {
+  return false;
+}
+};
+
+
+// RENDER OUR VIEW //
+
+var render = function() {
+
+}
+
+
+
+
+
